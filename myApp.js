8c6432d9d4faa1de1ca77318d60f96b2fcad8119
Whitneyy <whitneyyx77@gmail.com>
2024-12-30 18:43:53 -0500

    corrected spelling

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const app = express();
 const helmet = require('helmet');
 
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.frameguard({ action: 'DENY' }));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
