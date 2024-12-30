1dc52b59fb3a0f7bc6a1348505eac70cb1333939
Whitneyy <whitneyyx77@gmail.com>
2024-12-30 18:11:47 -0500

    Mounted helmet.hidePoweredBy correctly

--- myApp.js
+++ myApp.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+
+app.use(helmet.hidePoweredBy());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
 
 
 
